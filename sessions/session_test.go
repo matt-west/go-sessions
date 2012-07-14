ca15ff247a46e6b98ee0255170dc0a301e0a8b08
Matt West <matt.west@developercity.co.uk>
2012-07-14 19:45:01 +0100

    Moved everything into session.go file and removed old tests.

--- sessions/session_test.go
+++ sessions/session_test.go
@@ -8,54 +8,10 @@ import (
 	"testing"
 )
 
-// GetVar should retrieve a session variable
-func TestGetVar(t *testing.T) {
-	// Setup
-	ss := NewSessionStore()
-	s := ss.NewSession()
-	s.SetVar("foo", "bar")
+func TestSave(t *testing.T) {
 
-	// Test
-	x := s.GetVar("foo")
-
-	if x != "bar" {
-		t.Errorf("GetVar('foo') = %v, want 'bar'", x)
-	}
 }
 
-// SetVar should set a session variable
-func TestSetVar(t *testing.T) {
-	// Setup
-	ss := NewSessionStore()
-	s := ss.NewSession()
-	s.SetVar("foo", "bar")
-
-	// Test
-	val, ok := s.Values["foo"]
-
-	if !ok {
-		t.Error("Key not created")
-	}
-
-	if val != "bar" {
-		t.Errorf("Saved value does not match: expected %v, got '%v'", "'bar'", val)
-	}
-}
-
-// DestroyVar should remove a session variable
-func TestDestroyVar(t *testing.T) {
-	// Setup
-	ss := NewSessionStore()
-	s := ss.NewSession()
-	s.SetVar("foo", "bar")
-
-	if s.GetVar("foo") != "bar" {
-		t.Error("Test Setup Failed: Unable to set/get session variable")
-	}
-
-	// Test
-	s.DestroyVar("foo")
-	if s.GetVar("foo") != nil {
-		t.Error("Key not deleted")
-	}
+func TestLoad(t *testing.T) {
+	
 }
